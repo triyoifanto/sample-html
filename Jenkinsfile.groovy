ad8d59d1e0594d8dd34239328f3b32db695f96e4
Ifan <triyoifanto@users.noreply.github.com>
2017-02-22 17:52:49 +0800

    change group

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,7 +5,7 @@ node {
     env.AWS_DEFAULT_REGION = 'ap-southeast-1'
 	
 	def applicationName = 'fajar-app' //change me
-	def deploymentGroupName = 'fajar-app' // change me
+	def deploymentGroupName = 'diyon-triyo-group' // change me
 	def s3BucketName = 'deployment-cdc'
 	
 	//Cleanup workspace
